a000f109adbb07ec4cce18f4a3b90bed7b371d4b
Laura Spieker <ljspiek@gmail.com>
2020-02-11 19:53:18 -0600

    first commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import AboutMenu from './about-menu';
 
 export default function App(props) {
     return (
         <div className="app">
             {/* This should only show up on /about/foo, /about/bar, or /about/foo/bar */}
-            <AboutMenu />
+            <Route exact path='/about/foo' component={AboutMenu}/>
+            <Route exact path='/about/bar' component={AboutMenu}/>
+            <Route exact path='/about/foo/bar' component={AboutMenu}/>
+            
         </div>
     );
 }
